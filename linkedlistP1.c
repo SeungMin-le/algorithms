2500e0a0b58a7735a73f858c82329d733ced0e0c
SeungMin-le <dltmdals97x@naver.com>
2020-06-09 19:48:11 +0900

    LinkedListP1(2)

--- linkedlistP1.c
+++ linkedlistP1.c
@@ -3,52 +3,98 @@
 
 typedef struct node {
 	struct node* next;
+	struct node* prev;
 	int data;
-	
 }Node;
-typedef struct linkedList {
-	Node* first;
-	Node* last;
+typedef struct table {
+	Node* head;
+	Node* tail;
 	Node* index;
+	
 	int size;
-}*Link;
-Link createlinkedList() {
-	Link S = (Link)malloc(sizeof(struct linkedList));
-	S->first = NULL;
-	S->index = NULL;
-	S->size = 0;
+
+}*Table;
+Table createTable() {
+	Table T = (Table)malloc(sizeof(struct table));
+	T->head = NULL;
+	T->tail = NULL;
+	T->index = NULL;
+	T->size = 0;
+	return T;
 }
 Node* createNode(int item) {
 	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->prev = NULL;
 	newNode->next = NULL;
 	newNode->data = item;
+	return newNode;
+
 }
-Link makeYose(Link S,int n) {
-	int i = 1;
+void connectNode(Table T,int n) {
 	Node* newNode;
-	while (i <= n) {
-		if (S->size == 0) {
-			newNode = createNode(i);
-			S->first = newNode;
-			S->index = newNode;
-			S->last = newNode;
+	for (int i = 1; i <= n; i++) {
+
+		newNode = createNode(i);
+
+		if (T->size == 0) {
+			T->head = newNode;
+			T->tail = newNode;
 		}
 		else {
-			newNode = createNode(i);
-			S->last->next = newNode;
-			S->last = newNode;
+			newNode->prev = T->tail;
+			T->tail->next = newNode;
+			T->tail = newNode;
 			
+			newNode->next = T->head;
+			T->head->prev = T->tail;
 		}
-		i++;
-		S->size++;
+		T->size++;
 	}
-	return S;
+}
+int* yose(Table T,int n, int k) {
+	int indexNum=0;
+	int count = n;
+	int resultNumindex=0;
+	int* resultNum;
+	
+	resultNum = (int*)malloc(sizeof(int) * n);
+	connectNode(T, n);
+	T->index = T->head;
+	while (count==(k-1)) {
+		for (int i = 1; i <= k; i++) {
+			
+			indexNum = i;
+			if (indexNum == k) {
+				
+				resultNum[resultNumindex++] = T->index->data;
+				if (T->index == T->head)
+					T->head = T->head->next;
+				else if (T->index == T->tail)
+					T->tail = T->tail->prev;
+				
+				count--;
+				
+			}
+			T->index = T->index->next;
+			/*index로 k번째에 있는
+			정수 지우고 지워진 정수 배열에 저장하고 
+			지운 노드 앞뒤 연결*/
+		}
+	}
+	for (int i = 0; i < 7; i++) {
+		printf("%d", resultNum[i]);
+	}
+	return resultNum;
 }
 int main() {
-	Link s = createlinkedList();
-	Link horse = makeYose(s,7);
+
+	Table T = createTable();
+	int* result=yose(T, 7, 3);
+	
+	
 	for (int i = 0; i < 7; i++) {
-		printf("%d",horse->index->data);
-		horse = horse->index->next;
+		printf("%d",result[i]);
 	}
+	
+	return 0;
 }
